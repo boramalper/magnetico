3a12083b98bd9e874e34b4e61678b88313b28de2
Gabor Tanz <izolight@gmail.com>
2018-04-15 13:51:37 +0200

    Fix some syntax that won't work on postgres and cast where necessary.

--- pkg/persistence/postgres.go
+++ pkg/persistence/postgres.go
@@ -37,7 +37,7 @@ func makePostgresDatabase(url_ string) (Database, error) {
 }
 
 func (db *postgresDatabase) DoesTorrentExist(infoHash []byte) (bool, error) {
-	rows, err := db.conn.Query("SELECT 1 FROM torrents WHERE info_hash = ?;", infoHash)
+	rows, err := db.conn.Query("SELECT 1 FROM torrents WHERE info_hash = $1::BYTEA;", infoHash)
 	if err != nil {
 		return false, err
 	}
@@ -73,27 +73,26 @@ func (db *postgresDatabase) AddNewTorrent(infoHash []byte, name string, files []
 		return nil
 	}
 
-	res, err := tx.Exec(`
+	var lastInsertId int64
+	// we will insert the name as tsvector for easier searching and split on word boundaries
+	err = tx.QueryRow(`
 		INSERT INTO torrents (
 			info_hash,
 			name,
 			total_size,
-			discovered_on
-		) VALUES (?, ?, ?, ?)
+			discovered_on,
+			search
+		) VALUES ($1::BYTEA, $2::VARCHAR, $3, $4::TIMESTAMP, to_tsvector(regexp_replace(coalesce($2, ''), '[^\w]+', ' ', 'gi')))
 		ON CONFLICT
-		DO NOTHING;
-	`, infoHash, name, totalSize, time.Now().Unix())
+		DO NOTHING
+		RETURNING id;
+	`, infoHash, name, totalSize, time.Now()).Scan(&lastInsertId)
 	if err != nil {
-		return err
-	}
-
-	var lastInsertId int64
-	if lastInsertId, err = res.LastInsertId(); err != nil {
 		return fmt.Errorf("sql.Result.LastInsertId()!  %s", err.Error())
 	}
 
 	for _, file := range files {
-		_, err = tx.Exec("INSERT INTO files (torrent_id, Size, path) VALUES (?, ?, ?);",
+		_, err = tx.Exec("INSERT INTO files (torrent_id, Size, path) VALUES ($1, $2, $3::VARCHAR);",
 			lastInsertId, file.Size, file.Path,
 		)
 		if err != nil {
@@ -153,7 +152,7 @@ func (db *postgresDatabase) GetTorrent(infoHash []byte) (*TorrentMetadata, error
 			discovered_on,
 			(SELECT COUNT(1) FROM files WHERE torrent_id = torrents.id) AS n_files
 		FROM torrents
-		WHERE info_hash = ?;`,
+		WHERE info_hash = $1::BYTEA;`,
 		infoHash,
 	)
 	if err != nil {
@@ -178,7 +177,7 @@ func (db *postgresDatabase) GetFiles(infoHash []byte) ([]File, error) {
 	rows, err := db.conn.Query(`
 		SELECT size, path
 		FROM files
-		WHERE torrent_id = ?;`,
+		WHERE torrent_id = $1;`,
 		infoHash)
 	if err != nil {
 		return nil, err
@@ -208,12 +207,7 @@ func (db *postgresDatabase) Engine() databaseEngine {
 }
 
 func (db *postgresDatabase) setupDatabase() error {
-	// Ensure utf-8 encoding
-	_, err := db.conn.Exec(`
-		initdb -E UTF8;`)
-	if err != nil {
-		return fmt.Errorf("sql.DB.Exec (initdb): %s", err.Error())
-	}
+	// utf8 will be ensured by locale of postgres user
 
 	tx, err := db.conn.Begin()
 	if err != nil {
@@ -221,20 +215,8 @@ func (db *postgresDatabase) setupDatabase() error {
 	}
 	defer tx.Rollback()
 
+	// Create settings table first
 	_, err = tx.Exec(`
-		CREATE TABLE IF NOT EXISTS torrents (
-			id             SERIAL PRIMARY KEY,
-			info_hash      BYTEA NOT NULL UNIQUE,
-			name           VARCHAR NOT NULL,
-			total_size     BIGINT NOT NULL CHECK(total_size > 0),
-			discovered_on  TIMESTAMP NOT NULL CHECK(discovered_on > 0)
-		);
-		CREATE TABLE IF NOT EXISTS files (
-			id          SERIAL PRIMARY KEY,
-			torrent_id  INTEGER REFERENCES torrents ON DELETE CASCADE ON UPDATE RESTRICT,
-			size        BIGINT NOT NULL,
-			path        VARCHAR NOT NULL
-		);
 		CREATE TABLE IF NOT EXISTS settings (
 			name	VARCHAR UNIQUE,
 			value 	VARCHAR
@@ -249,10 +231,20 @@ func (db *postgresDatabase) setupDatabase() error {
 	if err != nil {
 		return fmt.Errorf("sql.Tx.Query (SCHEMA_VERSION): %s", err.Error())
 	}
+
 	var userVersion string
 	if rows.Next() != true {
-		return fmt.Errorf("sql.Rows.Next (SCHEMA_VERSION): SELECT value FROM settings WHERE name='SCHEMA_VERSION did not return any rows!")
+		// SCHEMA_VERSION does not exist
+		zap.L().Error("sql.Rows.Next (SCHEMA_VERSION): SCHEMA_VERSION did not return any rows!")
+		_, err = tx.Exec(`
+			INSERT INTO settings(name, value) VALUES ('SCHEMA_VERSION', '0');`)
+		rows, err = tx.Query("SELECT value FROM settings WHERE name='SCHEMA_VERSION';")
+		if err != nil {
+			return fmt.Errorf("sql.Tx.Query (SCHEMA_VERSION): %s", err.Error())
+		}
+		rows.Next()
 	}
+
 	if err = rows.Scan(&userVersion); err != nil {
 		return fmt.Errorf("sql.Rows.Scan (SCHEMA_VERSION): %s", err.Error())
 	}
@@ -260,6 +252,44 @@ func (db *postgresDatabase) setupDatabase() error {
 		return fmt.Errorf("sql.Rows.Close (SCHEMA_VERSION): %s", err.Error())
 	}
 
-	// TODO
+	// at the moment we just have 2 versions (0=no db or 1=current db)
+	switch userVersion {
+	case "0":
+		// initialise db
+		// we use postgres fulltext search in the torrents table
+		zap.L().Warn("Database was empty, initialising it")
+		_, err = tx.Exec(`
+		CREATE TABLE IF NOT EXISTS torrents (
+			id             	SERIAL PRIMARY KEY,
+			info_hash      	BYTEA NOT NULL UNIQUE,
+			name           	VARCHAR NOT NULL,
+			total_size     	BIGINT NOT NULL CHECK(total_size > 0),
+			discovered_on	TIMESTAMP NOT NULL,
+			updated_on		TIMESTAMP,
+			n_seeders		INTEGER CHECK ((updated_on IS NOT NULL AND n_seeders >= 0) OR (updated_on IS NULL AND n_seeders IS NULL)) DEFAULT NULL,
+			n_leechers		INTEGER CHECK ((updated_on IS NOT NULL AND n_leechers >= 0) OR (updated_on IS NULL AND n_leechers IS NULL)) DEFAULT NULL,
+			search			tsvector
+		);
+		CREATE TABLE IF NOT EXISTS files (
+			id          	SERIAL PRIMARY KEY,
+			torrent_id  	INTEGER REFERENCES torrents ON DELETE CASCADE ON UPDATE RESTRICT,
+			size        	BIGINT NOT NULL,
+			path        	VARCHAR NOT NULL,
+			is_readme		BOOLEAN DEFAULT NULL,
+			content		TEXT CHECK ((content IS NULL AND is_readme IS FALSE) OR (content IS NOT NULL AND is_readme IS TRUE) OR (content IS NULL AND is_readme IS NULL)) DEFAULT NULL
+		);
+		CREATE UNIQUE INDEX readme_index on files(torrent_id, is_readme);
+		CREATE INDEX torrents_idx ON torrents USING gin(search);
+		UPDATE settings SET value = '1' WHERE name = 'SCHEMA_VERSION';
+		`)
+		if err != nil {
+			return fmt.Errorf("sql.Tx.Exec (v0 -> v1): %s", err.Error())
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("sql.Tx.Commit: %s", err.Error())
+	}
+
 	return nil
 }
